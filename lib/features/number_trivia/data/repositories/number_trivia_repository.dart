7c587c2c523ef7f3676904e7e946c7c4fa096137
RENTKAR <amiyasatyam@gmail.com>
2023-11-01 06:16:28 +0530

    creating mocks for testing

--- lib/features/number_trivia/data/repositories/number_trivia_repository.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository.dart
@@ -3,6 +3,6 @@ import '../../../../core/error/failures.dart';
 import '../../domain/entities/number_trivia.dart';
 
 abstract class NumberTriviaRepository {
-  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int? number);
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
   Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
 }
--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -10,7 +10,7 @@ class GetConcreteNumberTrivia {
   GetConcreteNumberTrivia(this.repository);
 
   Future<Either<Failure, NumberTrivia>> execute({
-    required int? number,
+    required int number
   }) async {
     return await repository.getConcreteNumberTrivia(number);
   }
